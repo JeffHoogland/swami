94da8945e6b0d182d8b29ac131bd1d218bf614bf
Jeff Hoogland <jeffhoogland@linux.com>
2017-02-01 12:13:45 -0600

    Startup applications now lists application files. Slow and steady

--- swami_startupapps/swami_startupapps.py
+++ swami_startupapps/swami_startupapps.py
@@ -1,14 +1,16 @@
-#Swami skel module for the Swami Control Panel
+#Moksha startup applications module for the Swami Control Panel
 
 import os
+import gtk
 
 from efl.evas import EVAS_HINT_EXPAND, EVAS_HINT_FILL
 from efl import elementary
 from efl.elementary.button import Button
 from efl.elementary.box import Box
 from efl.elementary.icon import Icon
+from efl.elementary.image import Image
 
-from elmextensions import StandardButton
+from elmextensions import StandardButton, SearchableList
 
 EXPAND_BOTH = EVAS_HINT_EXPAND, EVAS_HINT_EXPAND
 EXPAND_HORIZ = EVAS_HINT_EXPAND, 0.0
@@ -17,6 +19,11 @@ FILL_HORIZ = EVAS_HINT_FILL, 0.5
 ALIGN_CENTER = 0.5, 0.5
 
 UserHome = os.path.expanduser("~")
+
+IconTheme = gtk.icon_theme_get_default()
+
+ApplicationPaths = [ "/usr/share/applications/",
+                "%s/.local/share/applications/"%UserHome]
 
 class SwamiModule(Box):
     def __init__(self, rent):
@@ -39,7 +46,7 @@ class SwamiModule(Box):
         self.icon.standard_set('system-run')
         self.icon.show()
         
-        self.mainBox = Box(self, size_hint_weight = EXPAND_BOTH)
+        self.mainBox = Box(self, size_hint_weight=EXPAND_BOTH, size_hint_align=FILL_BOTH)
         self.mainBox.show()
         
         buttonBox = Box(self, size_hint_weight = EXPAND_HORIZ, size_hint_align = FILL_BOTH)
@@ -50,6 +57,48 @@ class SwamiModule(Box):
         
         buttonBox.pack_end(buttonReturn)
         buttonBox.show()
+        
+        desktopFiles = []
+        
+        for ourPath in ApplicationPaths:
+            desktopFiles += [os.path.join(dp, f) for dp, dn, filenames in os.walk(ourPath) for f in filenames if os.path.splitext(f)[1] == '.desktop']
+        
+        self.applicationsList = applicationsList = SearchableList(self, size_hint_weight=EXPAND_BOTH, size_hint_align=FILL_BOTH)
+        #applicationsList.callback_item_focused_add(self.enableTZSelect)
+
+        applicationsToAdd = []
+
+        for d in desktopFiles:
+            with open(d) as desktopFile:
+                icon = None
+                for line in desktopFile:
+                    if line[:5] == "Name=":
+                        name = line[5:][:-1]
+                    
+                    if line[:5] == "Icon=":
+                        icon = line[5:]
+                    
+                
+                if icon:
+                    iconInfo = IconTheme.lookup_icon(icon.strip(), 48, 0)
+
+                    if iconInfo:
+                        iconObj = Image(self, file=iconInfo.get_filename(), size_hint_weight=EXPAND_BOTH, size_hint_align=FILL_BOTH)
+                    else:
+                        iconObj = Icon(self, standard="preferences-system", size_hint_weight=EXPAND_BOTH, size_hint_align=FILL_BOTH)
+                else:
+                    iconObj = Icon(self, standard="preferences-system", size_hint_weight=EXPAND_BOTH, size_hint_align=FILL_BOTH)
+                
+                applicationsToAdd.append([name, iconObj])
+                
+        applicationsToAdd.sort()
+        
+        for a in applicationsToAdd:
+            applicationsList.item_append(a[0], a[1])
+                
+        applicationsList.show()
+        
+        self.mainBox.pack_end(applicationsList)
         
         self.pack_end(self.mainBox)
         self.pack_end(buttonBox)
